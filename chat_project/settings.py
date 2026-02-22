feaad4d879875ff6969ee1a184dcc4589623ab7e
akhilsm97 <akhilsm97@gmail.com>
2026-02-22 13:22:01 +0530

    Fifth Commit

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -18,8 +18,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Add /apps paths to Python path
-sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 
 # Custom User model
 AUTH_USER_MODEL = "accounts.CustomUser"
