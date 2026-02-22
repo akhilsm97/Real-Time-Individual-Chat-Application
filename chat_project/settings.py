9769fd846edf3cab3b8bcb4636e9811d2454dcee
Akhil S <79699665+akhilsm97@users.noreply.github.com>
2026-02-22 13:12:13 +0530

    Update

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -44,8 +44,8 @@ CSRF_TRUSTED_ORIGINS = [
 INSTALLED_APPS = [
     "daphne",
     "channels",
-    "accounts",
-    "chat_app",
+    "apps.accounts",
+    "apps.chat_app",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
