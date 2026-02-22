63099b1154c3970484b0a7ac3ff46f8a3a397f30
akhilsm97 <akhilsm97@gmail.com>
2026-02-22 10:22:00 +0530

    Fourth Commit

--- chat_project/urls.py
+++ chat_project/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
     path('chat/', include('chat_app.urls')),
 ]
 if settings.DEBUG:
